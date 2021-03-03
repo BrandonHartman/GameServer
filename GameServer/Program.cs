7afeae01f6f714577c200530cf712ac974267859
Mulletzzz <BrandonHartman@users.noreply.github.com>
2021-03-02 20:11:09 -0500

    Add project files.

---
+++ GameServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+            // Console.WriteLine("Hello World");
+            Console.ReadKey();
+        }
+    }
+}
